93416488fefc851610a86d60fd645ffdab34f87f
yangkuikui <yangkuikui@gmail.com>
2021-10-14 14:38:29 +0900

    First commit

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require("express")
+const app = express()
+
+const router = require("./router") // require does 2 things. #1 execute that file immediately. #2 return whatever that file exports
+
+app.use(express.static("public"))
+app.set("views", "views")
+app.set("view engine", "ejs")
+
+app.get("/", function (req, res) {
+  res.render("home-guest")
+})
+
+app.listen(3000)
